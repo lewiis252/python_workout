693dc05372b4cbab474c27571d7134a858259e20
lewiis252 <krystianoleniacz@gmail.com>
2021-11-13 22:52:35 +0100

    1.0

---
+++ auto_screenshot.py
@@ -0,0 +1,34 @@
+import pyautogui
+import time
+import os
+
+num_of_minutes = int(input('Podaj czas trwania wykładu w minutach: '))
+lecture_title = ((input('Podaj tytuł wykładu: ')).replace(' ', '_'))
+wait_beetwen_screen = float(input('Co ile sekund wykonać screenshot? : '))
+
+if not os.path.exists(lecture_title):
+    os.mkdir(lecture_title)
+    print("Directory " , lecture_title ,  " Created ")
+else:
+    print("Taki folder " , lecture_title ,  " już istnieje!!!! Nadpiszę dane!!! Kontynuować?")
+    answer = input('[tak]/[nie]: ')
+    if answer == 'nie':
+        quit()
+
+i = 1
+status = True
+
+stop_cond = num_of_minutes*60/wait_beetwen_screen
+
+while status:
+    if i == stop_cond:
+        status = False
+    my_screen_shot = pyautogui.screenshot()
+    file_num = str(i)
+    my_screen_shot.save('{}\{}_{}.png'.format(lecture_title, str(lecture_title), file_num))
+    print('screenshot saved')
+
+    time.sleep(wait_beetwen_screen)
+
+    i+=1
+print('Pamiętaj skopiować screeny w bezpieczne miejsce, nie chcesz przypadkiem ich nadpisać!!!')
