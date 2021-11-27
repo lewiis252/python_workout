b7f024f6f37c9fc294cfd811508db06fb00e0a7f
lewiis252 <krystianoleniacz@gmail.com>
2021-11-27 21:46:34 +0100

    Added detection of identical previous screenshot. App do not take identical screenshot now.

--- auto_screenshot.py
+++ auto_screenshot.py
@@ -1,10 +1,15 @@
 import pyautogui
 import time
 import os
+import matplotlib.pyplot as plt
+import PIL
+from PIL import Image
+import numpy as np
 
 num_of_minutes = int(input('Podaj czas trwania wykładu w minutach: '))
 lecture_title = ((input('Podaj tytuł wykładu: ')).replace(' ', '_'))
 wait_beetwen_screen = float(input('Co ile sekund wykonać screenshot? : '))
+
 
 if not os.path.exists(lecture_title):
     os.mkdir(lecture_title)
@@ -20,18 +25,46 @@ status = True
 
 stop_cond = num_of_minutes*60/wait_beetwen_screen
 
+
 while status:
     if i == stop_cond:
         status = False
-    my_screen_shot = pyautogui.screenshot()
-    file_num = str(i)
-    my_screen_shot.save('{}\{}_{}.png'.format(lecture_title, str(lecture_title), file_num))
-    print('screenshot saved')
+    elif i == 1:
+        # time.sleep(3)  # wait 3 seconds before first screenshot
+        my_screen_shot = pyautogui.screenshot()
+        file_num = str(i)
+        my_screen_shot.save(f'{lecture_title}/{str(lecture_title)}_{file_num}.png')
+        print('Zrzut ekranu zapisany')
 
-    time.sleep(wait_beetwen_screen)
+        i += 1
+        time.sleep(wait_beetwen_screen)
 
-    i+=1
+
+    else:
+        my_screen_shot = pyautogui.screenshot()
+        file_num = str(i)
+        previous_file_num = str(i-1)
+        my_screen_shot.save(f'{lecture_title}/{str(lecture_title)}_{file_num}.png')
+
+        # compare if screenshot have changed
+        previous_img = (np.asarray(Image.open(f'{lecture_title}/{str(lecture_title)}_{previous_file_num}.png')))
+        current_img = (np.asarray(Image.open(f'{lecture_title}/{str(lecture_title)}_{file_num}.png')))
+
+        if np.array_equal(previous_img, current_img):
+            os.remove(f'{lecture_title}/{str(lecture_title)}_{file_num}.png')
+            print('Kopia poprzedniego zrzutu usunięta')
+
+        else:
+            print('Zrzut ekranu zapisany')
+            i += 1
+
+        time.sleep(wait_beetwen_screen)
+
+
 print('Pamiętaj skopiować screeny w bezpieczne miejsce, nie chcesz przypadkiem ich nadpisać!!!')
-time.sleep(30)
-a = input()
+
+
+
+
+
 
