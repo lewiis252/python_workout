a2c7122504ac8bac961163501d99abd761914b7d
lewiis252 <krystianoleniacz@gmail.com>
2022-02-20 18:36:43 +0100

    add files

--- regular_expres.py
+++ regular_expres.py
@@ -1,2 +1,6 @@
-print('hello there')
-print('heel2o')
+import re
+
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+mo = phoneNumRegex.search('My numbers is 415-555-4242')
+print(mo.group())
+print(mo.group(1))
